ac2e903a7b13bd3c62336ef412f2008e332f3db4
bhavyasreeKavuri <111688424+bhavyasreeKavuri@users.noreply.github.com>
2022-08-21 19:46:54 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,18 @@
+
+public class pattern{
+		public static void main(String[] args)  
+		{  
+		int rows=5;      
+		//inner loop  
+		for (int i= rows-1; i>=0 ; i--)  
+		{  
+		//outer loop  
+		for (int j=0; j<=i; j++)  
+		{  
+		//prints star and space  
+		System.out.print("*" + " ");  
+		}  
+		System.out.println();  
+		}  
+		}  
+}
